dcce861143cff46c99c34f6c417edd0d342b9209
Christoph <christoph.thees@gmx.de>
2021-06-09 14:09:03 +0200

    Update GameSettings

--- src/main/java/controller/UserInputController.java
+++ src/main/java/controller/UserInputController.java
@@ -14,13 +14,65 @@ public class UserInputController {
 
     @FXML
     private Button button1;
+    
+    @FXML
+    private Button background1;
+    @FXML
+    private Button background2;
+    @FXML
+    private Button background3;
+    
+    @FXML
+    private Button alien1;
+    @FXML
+    private Button alien2;
+    @FXML
+    private Button alien3;
+    
+    @FXML
+    private Button spaceship1;
+    @FXML
+    private Button spaceship2;
+    @FXML
+    private Button spaceship3;
 
     public void buttonClicked() throws Exception {
         Stage stage = (Stage) button1.getScene().getWindow();
         Pane root = FXMLLoader.load(getClass().getClassLoader().getResource("FXMLGameBoard.fxml"));
         stage.setScene(new Scene(root));
-
     }
+    
+    public void alien1Clicked() throws Exception{
+    	
+    }
+    public void alien2Clicked() throws Exception{
+    	
+    }
+    public void alien3Clicked() throws Exception{
+    	
+    }
+    
+    public void background1Clicked() throws Exception{
+    	
+    }
+    public void background2Clicked() throws Exception{
+    	
+    }
+    public void background3Clicked() throws Exception{
+    	
+    }
+    
+    public void spaceship1Clicked() throws Exception{
+    	
+    }
+    public void spaceship2Clicked() throws Exception{
+    	
+    }
+    public void spaceship3Clicked() throws Exception{
+    	
+    }
+    
+
 
     void keyboardInput() {
 
